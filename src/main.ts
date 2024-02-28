a9674951ba4a2f397a46093905db838643a00c15
unknown <tuantai09012000@gmail.com>
2024-02-28 16:09:28 +0700

    Fix bug deploy in main.ts file.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes';
-import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
-  app.use(cookieParser());
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true
